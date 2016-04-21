66a1c30ea2258ac425146088b4cad35a578f7b1f
armeo <armchockaumnuai@gmail.com>
2016-04-21 23:18:20 +0700

    Implement master/detail to view post

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -5,15 +5,26 @@ import {SpinnerComponent} from './spinner.component';
 
 @Component({
     templateUrl: 'app/posts.component.html',
+    styles: [`
+         .posts li { cursor: default; }
+         .posts li:hover { background: #ecf0f1; } 
+         .list-group-item.active, 
+         .list-group-item.active:hover, 
+         .list-group-item.active:focus { 
+             background-color: #ecf0f1;
+             border-color: #ecf0f1; 
+             color: #2c3e50;
+         }
+     `],
     providers: [PostService],
     directives: [SpinnerComponent]
 })
 export class PostsComponent implements OnInit {
     posts: any[];
     isLoading = true;
+    currentPost;
     
     constructor(private _service: PostService){
-        
     }
     
     ngOnInit(){
@@ -24,4 +35,7 @@ export class PostsComponent implements OnInit {
         );
     }
     
+    select(post){
+        this.currentPost = post;
+    }
 }
