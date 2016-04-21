84e4bb30070043f011eddefab070d558a3a48306
armeo <armchockaumnuai@gmail.com>
2016-04-21 14:39:39 +0700

    Finished step SPA

---
+++ app/posts.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    template: '<h1>Post</h1>'
+})
+export class PostsComponent {}
---
+++ app/users.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    template: '<h1>User</h1>'
+})
+export class UsersComponent{}
